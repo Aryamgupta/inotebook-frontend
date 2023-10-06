8132646b0b79dd140e5fdec6293d3a52b4e6de13
Aryamgupta <87194432+Aryamgupta@users.noreply.github.com>
2023-10-06 18:10:27 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
+import React, { Component } from 'react';
+import Navbar from './Components/Navbar';
+import News from './Components/News.js';
+// import React from "react";
 import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import About from './Components/About';
-import NodeState from './Context/Notes/NotesState';
-import MyAlert from './Components/MyAlert';
-import Login from './Components/Login';
-import SignUp from './Components/SignUp';
+import LoadingBar from 'react-top-loading-bar'
 
-function App() {
-  localStorage.setItem('token','');
-  return (
-    <>
-    <NodeState>
-      <Router>
-        <Navbar title="iNotebook" />
-        <MyAlert title = "Item Deleted" />
-        <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-        </div>
-      </Router>
-      </NodeState>
-    </>
-  )
+export class App extends Component {
+  apikey = process.env.REACT_APP_NEWS_API
+
+  state = {
+    progress : 0
+  }
+
+  setProgress = (progress)=> {
+    this.setState({progress : progress})
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+            <LoadingBar
+              color='red'
+              height={3}
+              progress= {this.state.progress}
+              // onLoaderFinished={() => setProgress(0)}
+            />
+          <Routes>
+            <Route exact path='/' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   category="general" country="in" />} />
+            <Route exact path='/business' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   key="business" category="business" country="in" />} />
+            <Route exact path='/entertainment' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   key="entertainment" category="entertainment" country="in" />} />
+            <Route exact path='/general' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   key="general" category="general" country="in" />} />
+            <Route exact path='/health' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   key="health" category="health" country="in" />} />
+            <Route exact path='/science' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   key="science" category="science" country="in" />} />
+            <Route exact path='/sports' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   key="sports" category="sports" country="in" />} />
+            <Route exact path='/technology' element={   <News  apikey = {this.apikey} setProgress = {this.setProgress}   key="technology" category="technology" country="in" />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
